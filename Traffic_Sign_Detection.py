821350f39802ee764d05c0085555296e7501f2c1
k1n3tic <46767335+k1n3tic@users.noreply.github.com>
2024-05-07 21:18:03 -0500

    Update Traffic_Sign_Detection.py

--- Traffic_Sign_Detection.py
+++ Traffic_Sign_Detection.py
@@ -1,26 +1,22 @@
-'''Traffic_Sign_Detection.py
-This script holds the camera loop for object detection, when running if sign detected car affected
-Update on the control_state.json file will happen when vehicle controlled from here
+'''
+QCardinals
+Traffic_Sign_Detection.py
+This script holds the camera loop for object detection, when running if sign detected car will react
 '''
 #imports
 import time
 import cv2
-import ultralytics.engine
-import ultralytics.engine.results
 from pal.products.qcar import QCarCameras
-
-import ultralytics
 from ultralytics import YOLO
 import torch
 from pal.products.qcar import QCar
-import json
-from filelock import Timeout, FileLock
+
 import numpy as np
 
 from hal.utilities.image_processing import ImageProcessing
 
 #opening midel and initializing variables
-model = YOLO('C:/Users/user/Downloads/George_Spring24_QCAR/Research_Resources/Research_Resources/src/SDCS/skills_activities/vehicle_control/best.pt')
+model = YOLO('best.pt')
 # Initial Setup
 runTime = 120.0  # Same runtime as the Vehicle Control Script
 sampleRate = 200
@@ -40,14 +36,7 @@ cameras = QCarCameras(
     enableRight=False,
 )
 
-def update_control_state(allow_prediction):
-    lock = FileLock("control_state.json.lock")
-    with lock:
-        with open('control_state.json', 'w') as file:
-            json.dump({'allow_prediction': allow_prediction}, file)
-
 #loop for camera interfacing and live detection
-# with QCarRealSense(mode='RGB, Depth') as myCam:
 with cameras:
     t0 = time.time()
     while time.time() - t0 < runTime:
@@ -68,8 +57,7 @@ with cameras:
         
         has_something = len(contours) >= 1
         print(has_something)
-        #         cv2.imshow(('CSI '+str(i)+':'), c.imageData)        
-        # cv2.waitKey(1)
+        LEDs = np.array([0, 0, 0, 0, 0, 0, 1, 1])
         # Object detection
         results = model(frame)
         det_cls = results[0].boxes.cls
@@ -81,14 +69,14 @@ with cameras:
         # x_val = int(nump[0,0])
         # y_val = int(nump[0,3])
         if torch.equal(det_cls, torch.tensor([2.0])):
-            if frames_since_last_stop_stop  >= 100:  # Ensure 200 frames have passed since last stop
+            if frames_since_last_stop_stop  >= 50:  # Ensure 200 frames have passed since last stop
                 if nump[0,1] > 50:
                     with QCar(frequency=200) as myCar:
                         with QCar(frequency=sampleRate) as myCar:
                             t1 = time.time()
                             # update_control_state(False)
                             while time.time() - t1 < 3: #Stop 3 seconds
-                                myCar.write(0,0,None)
+                                myCar.write(0,0,LEDs)
                             frames_since_last_stop_stop  = 0  # Reset frame counter after stopping
                             # update_control_state(True)
 
@@ -101,17 +89,14 @@ with cameras:
         # if torch.equal(det_cls, torch.tensor([0.0])) and has_something:
         if torch.equal(det_cls, torch.tensor([0.0])) and int(nump[0,3]) > 23:
             det_counter_red +=1
-                # Ensure 200 frames have passed since last stop
-            if nump[0,1] > 100 and nump[0,0] > 250 :#and det_counter_red >= 10:
+            # Ensure 200 frames have passed since last stop
+            if nump[0,1] > 100 and nump[0,0] > 250 : #and det_counter_red >= 10:
                 with QCar(frequency=200) as myCar:
                     with QCar(frequency=sampleRate) as myCar:
                         t1 = time.time()
                         det_counter_red = 0
-                        # update_control_state(False)
+                        # loop to write stop to car for red light
                         while time.time() - t1 < 2.5: #Stop 3 seconds
-                            myCar.write(0,0,None)
-                        # update_control_state(True)
+                            myCar.write(0,0,LEDs)
 
-                        # t2 = time.time()
-                        # while time.time() - t2 < 0.3:
-                        #     myCar.write(0.1,0, LEDs)
+        
